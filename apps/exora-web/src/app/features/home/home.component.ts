25007e6cf119fb3c3ce1bccf8fe270ce85a5b16c
seconduniQueee <plotnikovfw@gmail.com>
2024-11-24 21:42:34 +0200

    Chore: cleanup

--- apps/exora-web/src/app/features/home/home.component.ts
+++ apps/exora-web/src/app/features/home/home.component.ts
@@ -1,25 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { AuthService } from "../../core/auth/auth.service";
 import { DialogService } from "../../core/dialog/dialog.service";
-import { DatePickerComponent } from "@exora-web/shared/ui/date-picker/date-picker.component";
-import { DatePipe } from "@angular/common";
-import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
-import { FormSubmitDirective } from "@exora-web/shared/directives";
 
 @Component({
   templateUrl: "home.component.html",
   styleUrl: "home.component.scss",
-  providers: [DialogService, AuthService, DatePickerComponent],
+  providers: [DialogService, AuthService],
   standalone: true,
-  imports: [DatePickerComponent, DatePipe, FormSubmitDirective, ReactiveFormsModule],
 })
-export class HomeComponent implements OnInit {
-  dateForm: FormGroupTyped<DateForm>;
-
-  constructor(
-    private authService: AuthService,
-    private fb: FormBuilder,
-  ) {}
+export class HomeComponent {
+  constructor(private authService: AuthService) {}
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -28,22 +18,4 @@ export class HomeComponent implements OnInit {
   get userName(): string {
     return this.authService.userInfo.firstName;
   }
-
-  ngOnInit(): void {
-    this.initDateForm();
-  }
-
-  submitForm(): void {
-    console.log(this.dateForm.value);
-  }
-
-  private initDateForm(): void {
-    this.dateForm = this.fb.group({
-      testDate: [null, Validators.required],
-    }) as FormGroupTyped<DateForm>;
-  }
-}
-
-interface DateForm {
-  testDate: Date;
 }
